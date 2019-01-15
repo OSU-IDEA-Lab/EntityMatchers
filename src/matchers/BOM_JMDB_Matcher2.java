dfa98ef5c96b7d3e4f4f32dfc7f6bfe862fa7fbc
Jose Picado <jpicado@gmail.com>
2019-01-14 16:31:06 -0800

    Changed CSV parser in a macher.

--- src/matchers/BOM_JMDB_Matcher2.java
+++ src/matchers/BOM_JMDB_Matcher2.java
@@ -3,7 +3,6 @@ package matchers;
 import static org.simmetrics.builders.StringMetricBuilder.with;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -24,9 +23,10 @@ import org.simmetrics.StringMetric;
 import org.simmetrics.metrics.SmithWatermanGotoh;
 import org.simmetrics.simplifiers.Simplifiers;
 
+import com.opencsv.CSVReader;
+
 import castor.similarity.SimilarValue;
 import castor.utils.TimeWatch;
-import utils.CSVParser;
 
 public class BOM_JMDB_Matcher2 {
 	
@@ -60,25 +60,50 @@ public class BOM_JMDB_Matcher2 {
 		Map<String,Set<String>> list2Blocks = new HashMap<String,Set<String>>();
 		
 		BufferedReader br = null;
+		CSVReader reader = null;
 		try {
-			// Read file 1
-			br = new BufferedReader(new FileReader(new File(file1)));
-			String line;
-			// Skip header
-			line = br.readLine();
-			while ((line = br.readLine()) != null) {
-				List<String> parsed = CSVParser.parseLine(line);
-				String title = parsed.get(0);
-				list1.add(title);
-			}
+//			// Read file 1
+//			br = new BufferedReader(new FileReader(new File(file1)));
+//			String line;
+//			// Skip header
+//			line = br.readLine();
+//			while ((line = br.readLine()) != null) {
+//				List<String> parsed = CSVParser.parseLine(line);
+//				String title = parsed.get(0);
+//				list1.add(title);
+//			}
+//			
+//			// Read file 2
+//			br = new BufferedReader(new FileReader(new File(file2)));
+//			// Skip header
+//			line = br.readLine();
+//			while ((line = br.readLine()) != null) {
+//				List<String> parsed = CSVParser.parseLine(line);
+//				String title = parsed.get(1);
+//				String[] tokens = title.split("[\\p{Punct}\\s]+");
+//				for (String token : tokens) {
+//					if (!stopWordSet.contains(token.toLowerCase())) {
+//						if (!list2Blocks.containsKey(token)) {
+//							list2Blocks.put(token, new HashSet<String>());
+//						}
+//						list2Blocks.get(token).add(title);
+//					}
+//				}
+//			}
 			
-			// Read file 2
-			br = new BufferedReader(new FileReader(new File(file2)));
-			// Skip header
-			line = br.readLine();
-			while ((line = br.readLine()) != null) {
-				List<String> parsed = CSVParser.parseLine(line);
-				String title = parsed.get(1);
+			
+			// Read file 1
+			reader = new CSVReader(new FileReader(file1));
+		    String [] nextLine;
+		    while ((nextLine = reader.readNext()) != null) {
+		    	String title = nextLine[0];
+		    	list1.add(title);
+		    }
+		    
+		    // Read file 2
+		    reader = new CSVReader(new FileReader(file2));
+		    while ((nextLine = reader.readNext()) != null) {
+		    	String title = nextLine[1];
 				String[] tokens = title.split("[\\p{Punct}\\s]+");
 				for (String token : tokens) {
 					if (!stopWordSet.contains(token.toLowerCase())) {
@@ -88,7 +113,8 @@ public class BOM_JMDB_Matcher2 {
 						list2Blocks.get(token).add(title);
 					}
 				}
-			}
+		    }
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -97,6 +123,13 @@ public class BOM_JMDB_Matcher2 {
 			if (br != null) {
 				try {
 					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (reader != null) {
+				try {
+					reader.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -144,7 +177,7 @@ public class BOM_JMDB_Matcher2 {
 			}
 			
 			for (String movie2 : matches) {
-//				System.out.println(movie1+" - "+movie2);
+//				System.out.println(movie1+"-"+movie2);
 				sb.append("\"" + movie1.replace("\"", "\"\"") + "\", \"" + movie2.replace("\"", "\"\"") + "\"\n");
 			}
 		}
